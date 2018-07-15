d554c926d98f32b85fb0dac0dbd4e7d415d6918c
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-07-15 16:34:28 -0700

    Setting up endpoints

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+mongoose.Promise = global.Promise;
+
 const {PORT, DATABASE_URL} = require('./config');
 const {Blog} = require('./models');
+
+app.use(express.json());
 
 //GET REQUESTS /posts or /posts/:id
 //need to return JSON
@@ -15,6 +19,9 @@ const {Blog} = require('./models');
 // 	"author": "some person",
 // 	"created": "123321534"
 // }
+app.get("./posts", (req, res) => {});
+
+app.get("./posts/:id", (req, res) => {});
 
 //POST REQUESTS /posts
 //need to take a json
@@ -29,13 +36,16 @@ const {Blog} = require('./models');
 // 	}
 // }
 //should return the same way the GET requests look
+app.post("./posts", (req, res) => {});
 
 //PUT REQUESTS /posts/:id
 //needs to take a json
 //has to have id in body and the url path
 //if they don't match return 400
 //should return updated object and 200 code
+app.put("./posts/:id", (req, res) => {});
 
 //DELETE REQUESTS /posts/:id
 //allows you to delete with a given id
 //responds 204
+app.delete("./posts/:id", (req, res) => {});
